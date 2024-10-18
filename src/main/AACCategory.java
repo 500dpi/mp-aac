113e80f811a8f9c99a30d22683b951a0899c19ae
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-10-17 22:55:20 -0500

    AACCategory implementation + stubs for AACMappings

---
+++ src/main/AACCategory.java
@@ -0,0 +1,125 @@
+import java.util.NoSuchElementException;
+import edu.grinnell.csc207.util.KeyNotFoundException;
+import edu.grinnell.csc207.util.NullKeyException;
+import edu.grinnell.csc207.util.AssociativeArray;
+
+/**
+ * Represents the mappings for a single category of items that should
+ * be displayed.
+ *
+ * @author Catie Baker
+ * @author Sara Jaljaa
+ *
+ * @course CSC-207-01
+ *
+ */
+public class AACCategory implements AACPage {
+
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+	/**
+	 * A string to hold the category name.
+	 */
+	String category;
+
+	/**
+	 * An AssociativeArray to hold the image & text pair of a category.
+	 */
+	AssociativeArray<String, String> items;
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+	
+	/**
+	 * Creates a new empty category with the given name.
+	 *
+	 * @param name
+	 * 		The name of the category.
+	 */
+	public AACCategory(String name) {
+		this.category = name;
+		this.items = new AssociativeArray<String, String>();
+	} // AACCategory(String)
+
+  // +---------+-----------------------------------------------------
+  // | Methods |
+  // +---------+
+	
+	/**
+	 * Adds the image location, text pairing to the category.
+	 *
+	 * @param imageLoc
+	 * 		The location of the image.
+	 * @param text
+	 * 		The text that image should speak.
+	 */
+	public void addItem(String imageLoc, String text) {
+		try {
+			this.items.set(imageLoc, text);
+		} catch (NullKeyException e) {
+			return;
+		} // try/catch
+	} // addItem(String, String)
+
+	/**
+	 * Returns an array of all the images in the category.
+	 *
+	 * @return
+	 * 		The array of image locations; if there are no images,
+	 *		it should return an empty array.
+	 */
+	public String[] getImageLocs() {
+
+		// Create an array to hold all of the file location strings
+		String[] locations = new String[this.items.size()];
+
+		// Iterate and assign all image locations to the new array's indices
+		for (int i = 0; i < this.items.size(); i++) {
+			// FIX: implement me!
+		} // for
+		return locations;
+	} // getImageLocs()
+
+	/**
+	 * Returns the name of the category.
+	 *
+	 * @return
+	 * 		The name of the category.
+	 */
+	public String getCategory() {
+		return this.category;
+	} // getCategory()
+
+	/**
+	 * Returns the text associated with the given image in this category.
+	 *
+	 * @param imageLoc
+	 * 		The location of the image.
+	 * @return
+	 * 		The text associated with the image.
+	 * @throws NoSuchElementException
+	 *		If the image provided is not in the current category.
+	 */
+	public String select(String imageLoc) {
+		try {
+			return this.items.get(imageLoc);
+		} catch (KeyNotFoundException e) {
+			throw new NoSuchElementException("Error: Image does not exist in category.");
+		} // try/catch
+	} // select(String)
+
+	/**
+	 * Determines if the provided image is stored in the category.
+	 *
+	 * @param imageLoc
+	 * 		The location of the category.
+	 * @return
+	 * 		True if it is in the category, false otherwise.
+	 */
+	public boolean hasImage(String imageLoc) {
+		return this.items.hasKey(imageLoc);
+	} // hasImage(String)
+} // class AACCategory
---
+++ src/main/AACMappings.java
@@ -0,0 +1,144 @@
+/**
+ * Creates a set of mappings of an AAC that has two levels,
+ * one for categories and then within each category, it has
+ * images that have associated text to be spoken. This class
+ * provides the methods for interacting with the categories
+ * and updating the set of images that would be shown and handling
+ * an interactions.
+ *
+ * @author Catie Baker
+ * @author Sara Jaljaa
+ *
+ * @course CSC-207-01
+ *
+ */
+public class AACMappings implements AACPage {
+	
+	/**
+	 * Creates a set of mappings for the AAC based on the provided
+	 * file. The file is read in to create categories and fill each
+	 * of the categories with initial items. The file is formatted as
+	 * the text location of the category followed by the text name of the
+	 * category and then one line per item in the category that starts with
+	 * > and then has the file name and text of that image
+	 *
+	 * for instance:
+	 * img/food/plate.png food
+	 * >img/food/icons8-french-fries-96.png french fries
+	 * >img/food/icons8-watermelon-96.png watermelon
+	 * img/clothing/hanger.png clothing
+	 * >img/clothing/collaredshirt.png collared shirt
+	 *
+	 * represents the file with two categories, food and clothing
+	 * and food has french fries and watermelon and clothing has a
+	 * collared shirt.
+	 *
+	 * @param filename
+	 * 		The name of the file that stores the mapping information.
+	 */
+	public AACMappings(String filename) {
+		return; // STUB
+	} // AACMappings(String)
+	
+	/**
+	 * Given the image location selected, it determines the action to be
+	 * taken. This can be updating the information that should be displayed
+	 * or returning text to be spoken. If the image provided is a category,
+	 * it updates the AAC's current category to be the category associated
+	 * with that image and returns the empty string. If the AAC is currently
+	 * in a category and the image provided is in that category, it returns
+	 * the text to be spoken.
+	 *
+	 * @param imageLoc
+	 * 		The location where the image is stored.
+	 * @return
+	 * 		If there is text to be spoken, it returns that information, otherwise
+	 * 		it returns the empty string.
+	 * @throws NoSuchElementException
+	 * 		If the image provided is not in the current category.
+	 */
+	public String select(String imageLoc) {
+		return null; // STUB
+	} // select(String)
+	
+	/**
+	 * Provides an array of all the images in the current category.
+	 *
+	 * @return
+	 * 		The array of images in the current category; if there are no images,
+	 * 		it should return an empty array.
+	 */
+	public String[] getImageLocs() {
+		return null; // STUB
+	} // getImageLocs()
+	
+	/**
+	 * Resets the current category of the AAC back to the default
+	 * category.
+	 */
+	public void reset() {
+		return; // STUB
+	} // reset()
+	
+	/**
+	 * Writes the ACC mappings stored to a file. The file is formatted as
+	 * the text location of the category followed by the text name of the
+	 * category and then one line per item in the category that starts with
+	 * > and then has the file name and text of that image
+	 *
+	 * for instance:
+	 * img/food/plate.png food
+	 * >img/food/icons8-french-fries-96.png french fries
+	 * >img/food/icons8-watermelon-96.png watermelon
+	 * img/clothing/hanger.png clothing
+	 * >img/clothing/collaredshirt.png collared shirt
+	 *
+	 * represents the file with two categories, food and clothing
+	 * and food has french fries and watermelon and clothing has a
+	 * collared shirt.
+	 *
+	 * @param filename
+	 * 		The name of the file to write the AAC mapping to.
+	 */
+	public void writeToFile(String filename) {
+		return; // STUB
+	} // writeToFile(String)
+	
+	/**
+	 * Adds the mapping to the current category (or the default category if
+	 * that is the current category).
+	 *
+	 * @param imageLoc
+	 * 		The location of the image.
+	 * @param text
+	 * 		The text associated with the image.
+	 */
+	public void addItem(String imageLoc, String text) {
+		return; // STUB
+	} // addItem(String, String)
+	
+	/**
+	 * Gets the name of the current category.
+	 *
+	 * @return
+	 * 		The current category or the empty string if
+	 * 		on the default category.
+	 */
+	public String getCategory() {
+		return null; // STUB
+	} // getCategory()
+	
+	/**
+	 * Determines if the provided image is in the set of images that
+	 * can be displayed and false otherwise.
+	 *
+	 * @param imageLoc
+	 * 		The location of the category.
+	 * @return
+	 * 		True if it is in the set of images that
+	 * 		can be displayed, false otherwise.
+	 */
+	public boolean hasImage(String imageLoc) {
+		return false;
+	} // hasImage(String)
+} // AACMappings class
